667ffc9e5ad241663685c158b85adc2d7ce7380d
braddy <braddygabriel@gmail.com>
2023-08-15 01:12:34 -0600

    modificacion de modal

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,25 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var modalContainer = document.getElementById("modal-container");
-    var closeButton = document.getElementById("close");
-    var modalImage = document.querySelector(".modal-content img");
-  
-    function showModal() {
-      modalContainer.style.display = "block";
-      document.documentElement.style.overflow = "hidden";
+  var modalContainer = document.getElementById("modal-container");
+  var closeButton = document.getElementById("close");
+  var modalImage = document.querySelector(".modal-content img");
+
+  function showModal() {
+    modalContainer.style.display = "block";
+    document.documentElement.style.overflow = "hidden";
+  }
+
+  function closeModal() {
+    modalContainer.style.display = "none";
+    document.documentElement.style.overflow = "auto";
+  }
+
+  setTimeout(showModal, 3000);
+
+  modalContainer.addEventListener("click", function(e) {
+    if (e.target === modalContainer || e.target === modalImage) {
+      closeModal();
     }
-  
-    function closeModal() {
-      modalContainer.style.display = "none";
-      document.documentElement.style.overflow = "auto";
-    }
-  
-    setTimeout(showModal, 3000);
-  
-    modalContainer.addEventListener("click", function(e) {
-      if (e.target === modalContainer || e.target === modalImage) {
-        closeModal();
-      }
-    });
-  
-    closeButton.addEventListener("click", closeModal);
   });
+
+  closeButton.addEventListener("click", closeModal);
+});
